b9c2de2a3d0d617411f5017e811768cd120e479f
Ondrej <Sam256@web.de>
2013-07-22 10:55:33 +0200

    adds 'pdf' to supported template file extensions

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
  
+// add pdf to supported template file extensions
+$GLOBALS['TL_CONFIG']['templateFiles'] .= ',pdf';
 
 $GLOBALS['TL_HOOKS']['generatePage'][] = array('PageMimikry', 'mimikryGeneratePage');
